424c9c708554ff0bb6f4702a9257fc73fcdd39f7
A D E L M A H M O U D <adelam578568@gmail.com>
2024-08-28 19:14:47 +0300

    Update Speech-to-Text.py

--- Speech-to-Text.py
+++ Speech-to-Text.py
@@ -103,12 +103,7 @@ def Transcription():
                 else:
                     print("Please enter file format srt or vtt!")
         save_subtitle_file()
-    except FileNotFoundError as file_error:
-        print("-" * 40)
-        print(f"{file_error}")
-        print("Please enter the path of the file like this G:/Path/file_name.mp3")
-        print("If you do not know the path of the files, please move the audio file to the same folder where the script runs")
-        print("-" * 40)
+        
     except TimeoutError as time_error:
         print(f"->{time_error}")
     except ValueError as v_error:
